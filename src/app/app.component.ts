038ed8715c1e4a27ef9981da957c4442cdb2b173
Jose <tigrezhito@gmail.com>
2019-03-19 19:54:29 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { StatusBar } from '@ionic-native/status-bar';
 import { Storage } from '@ionic/storage';
 import {IonicStorageModule} from '@ionic/storage';
 import { MyApp } from './app.component';
